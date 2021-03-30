b694be83b941b77aee5382894158a188be816a7a
Bryan Roof <broof716@gmail.com>
2021-03-29 17:05:48 -0700

    adding icons to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a HULU Clone</h1>
+      <Header />
+
+      {/* Navbar */}
+
+      {/* Results */}
     </div>
   );
 }
