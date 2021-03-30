ccaf269a3cbb52dfe960b813fa7e594a6b693112
Bryan Roof <broof716@gmail.com>
2021-03-29 17:51:50 -0700

    Setting up Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import Nav from './components/Nav';
 
 
 
@@ -9,7 +10,7 @@ function App() {
     <div className="app">
       <Header />
 
-      {/* Navbar */}
+      <Nav />
 
       {/* Results */}
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../style/Nav.css";
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <h1>I am the Navbar</h1>
+    </div>
+  )
+}
+
+export default Nav;
